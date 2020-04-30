ca1448df4977aeb04d3a3ad704f4b1459093eca3
DlieBG <pi@pi4>
2020-04-30 16:15:29 +0200

    Neuer Slave

---
+++ homecontrol/slave.py
@@ -0,0 +1,95 @@
+import threading, http.server, socketserver
+import RPi.GPIO as GPIO
+import http.client
+import ast
+from collections import defaultdict
+from time import sleep
+
+global values
+
+class Values:
+    def __init__(self):
+        self.values = defaultdict(lambda : "0")
+
+    def set(self, key, value):
+        updater = http.client.HTTPConnection('http://192.168.0.3', 8080, timeout=20)
+        updater.request("GET", "/set?"+str(key)+"="+str(value))
+        updater.close()      
+        
+        self.values[str(key)]=str(value)
+
+    def get(self, key):
+        return self.values[str(key)]
+        
+    def switch(self, key):
+        if values.get(key) is "0":
+            values.set(key, "1")
+            return
+        if values.get(key) is "1":
+            values.set(key, "0")
+
+class Client:
+    def __init__(self):
+        connection = http.client.HTTPConnection('192.168.0.3', 8080, timeout=20)
+        while True:
+            connection.request("GET", "/get")
+            values.values = ast.literal_eval(connection.getresponse())
+            sleep(2)
+
+class Inputs:
+    def __init__(self):
+        self.setupInputs()
+        while True:
+            self.getInputs()
+            sleep(0.01)
+           
+    def setupInputs(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)        
+
+    def getInputs(self):
+        if GPIO.input(4) == GPIO.HIGH:
+            values.switch(1)
+            while GPIO.input(4) == GPIO.HIGH:
+                sleep(0.05)
+
+class Outputs:
+    def __init__(self):
+        self.setupOutputs()
+        while True:
+            self.setOutputs()
+            sleep(0.01)
+
+    def setupOutputs(self):
+        GPIO.setup(15, GPIO.OUT)
+
+    def setOutputs(self):
+        self.set(15, values.get(1))
+        
+    def set(self, gpio, value):
+        if value is "0":
+	        GPIO.output(gpio, GPIO.HIGH)
+        if value is "1":
+            GPIO.output(gpio, GPIO.LOW)
+
+values = Values()
+
+client = threading.Thread(target=Client)
+client.daemon = True
+client.start()
+
+inputs = threading.Thread(target=Inputs)
+inputs.daemon = True
+inputs.start()
+
+outputs = threading.Thread(target=Outputs)
+outputs.daemon = True
+outputs.start()
+
+
+#values.set("s1", "1")
+
+
+while True:
+    print(dict(values.values))
+    sleep(2)
