9e893da90831b5166f2b0c2d91d6b8f944e01d10
Benedikt <mail@benedikt-schwering.de>
2020-06-19 12:29:54 +0200

    Update index.php

--- NFCstamp/index.php
+++ NFCstamp/index.php
@@ -1,6 +1,7 @@
 <?php
     $dir = '/home/pi/NAS/Public/Daten/Christoph/NFC_Stempelsystem/';
-    $file = $dir.date('Y-m-d').'.csv';
+    $file = $dir.'Stempeldatei.csv';
 
-    file_put_contents($file, date('H:i').';'.$_GET['name'], FILE_APPEND);
+    file_put_contents($file, date('Y-m-d H:i:s').';'.$_GET['name'].'
+', FILE_APPEND);
 ?>
